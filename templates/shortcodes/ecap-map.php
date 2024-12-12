ba3220b6806006febb7966fd7bb64f65e6851c89
Ramiz Afzal <m.ramiz.afzal@gmail.com>
2024-12-12 20:38:21 +0500

    map and status layput changed

--- templates/shortcodes/ecap-map.php
+++ templates/shortcodes/ecap-map.php
@@ -1,7 +1,7 @@
 <div class="ecap-map-main">
-    <div class="ecap-status-wrapper"></div>
     <div class="ecap-map-wrapper">
         <div class="ecap-map" data-map-id="<?= $id ? $id : ''; ?>"></div>
         <div class="ecap-autocomplete"></div>
     </div>
+    <div class="ecap-status-wrapper"></div>
 </div>
